aa765a2fae362994495ea5391e833c4e15471722
Ryan H. Lewis <ryan.lewis@ayasdi.com>
2015-06-20 16:56:42 -0700

    using catch for unit tests.

--- test_tree.cpp
+++ test_tree.cpp
@@ -1,17 +1,23 @@
-#include "tree.hpp"
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
 #include <iostream>
+#include "tree.hpp"
 
 namespace ml = ayasdi::ml;
 
-int main(int argc, char** argv){
+TEST_CASE("Tree Tests", "[decision_tree]"){
  ml::decision_tree t;
- auto r = t.insert_root();
  auto g = t.root();
-
- std::cout << (g == r) << std::endl;
- auto children = t.insert_children( g);
-
- auto& left_child = std::get<0>( children);
- auto& right_child = std::get<1>( children);
- return 0;
+ SECTION("Root Nodes"){
+  auto r = t.insert_root();
+  REQUIRE(g == r);
+ }
+ SECTION("Children"){
+  auto children = t.insert_children( g);
+  auto& left_child = std::get<0>( children);
+  auto& right_child = std::get<1>( children);
+  t[  g.left_child()] == left_child;
+  t[ g.right_child()] == right_child;
+ }
 }
