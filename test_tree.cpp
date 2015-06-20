f7f65687931b6ce0ba7bcd281629f482cb6c774e
Ryan H. Lewis <ryan.lewis@ayasdi.com>
2015-06-20 15:37:19 -0700

    space.

--- test_tree.cpp
+++ test_tree.cpp
@@ -4,15 +4,14 @@
 namespace ml = ayasdi::ml;
 
 int main(int argc, char** argv){
-	ml::decision_tree t;
-	auto r = t.insert_root();
- 	auto g = t.root();
+ ml::decision_tree t;
+ auto r = t.insert_root();
+ auto g = t.root();
 
-	std::cout << (g == r) << std::endl;
-	auto children = t.insert_children( g);
+ std::cout << (g == r) << std::endl;
+ auto children = t.insert_children( g);
 
-	auto& left_child = std::get<0>( children);
-	auto& right_child = std::get<1>( children);
-
-	return 0;
+ auto& left_child = std::get<0>( children);
+ auto& right_child = std::get<1>( children);
+ return 0;
 }
