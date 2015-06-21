9c4dce007c47ca448f3a58a6f254f21e2a2c49d8
Ryan H. Lewis <ryan.lewis@ayasdi.com>
2015-06-20 17:03:43 -0700

    fixed tabbing for insert

--- tree.hpp
+++ tree.hpp
@@ -30,7 +30,7 @@ class decision_tree {
  public:
  typedef dtree_node node;
  
- decision_tree( int reserve_max_size=500) { tree_nodes.reserve( reserve_max_size); }
+ decision_tree( int reserve_max_size=500){ tree_nodes.reserve( reserve_max_size); }
 
  node& operator[]( std::size_t i){ return tree_nodes[ i]; }
 
@@ -65,10 +65,10 @@ class decision_tree {
  
  void reserve( int size){ tree_nodes.reserve( size); }
  
- private:
-     node& insert(){
-         tree_nodes.emplace_back(); 
-         return tree_nodes.back();
+private:
+ node& insert(){
+    tree_nodes.emplace_back(); 
+    return tree_nodes.back();
  }
  std::vector<node> tree_nodes; 
 }; //end class decision_tree
