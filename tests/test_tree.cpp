e869f8c194df82b1d8d3a1ee5ef71f3366877f0b
Ryan H. Lewis <ryan.lewis@ayasdi.com>
2015-06-22 21:02:07 -0700

    fix for insert_children and added some tests.

--- tests/test_tree.cpp
+++ tests/test_tree.cpp
@@ -8,17 +8,48 @@
 namespace ml = ayasdi::ml;
 
 TEST_CASE("Tree Tests", "[decision_tree]"){
- ml::decision_tree< double> t;
+ typedef ml::decision_tree< double> tree; 
+ typedef tree::node tree_node;
+ tree t;
  auto g = t.root();
- SECTION("Root Nodes"){
+ SECTION("Insert Root Node"){
   auto r = t.insert_root();
   REQUIRE(g == r);
  }
- SECTION("Children"){
+ SECTION("Insert Children"){
   auto children = t.insert_children( g);
   auto& left_child = std::get<0>( children);
   auto& right_child = std::get<1>( children);
   REQUIRE( t[  g.left_child_index()] == left_child);
   REQUIRE( t[ g.right_child_index()] == right_child);
+ }
+ SECTION("Node Functions"){
+    SECTION("Constructors"){
+     tree_node n;
+     SECTION("Default Constructor"){
+        REQUIRE( n.split_ == 0);
+        REQUIRE( n.split_value_ ==0);
+        CHECK( n.is_leaf());
+        CHECK_FALSE( n.is_not_leaf());
+     }
+     n.split_ = 3;
+     n.split_value_ = 3.14;
+     SECTION("Copy Constructor"){
+         tree_node c( n);
+         REQUIRE( c.split_ == n.split_);
+         REQUIRE( c.split_value_ == n.split_value_);
+         CHECK( c.is_leaf());
+         CHECK_FALSE( c.is_not_leaf());
+     }
+     SECTION("Move Constructor"){
+        tree_node c( std::move( n));
+        REQUIRE( c.split_ == 3);
+        REQUIRE( c.split_value_ == 3.14);
+        CHECK( c.is_leaf());
+        CHECK_FALSE( c.is_not_leaf());
+    }
+    //SECTION("Equality Operators"){
+    //}
+  }
  }
 }
